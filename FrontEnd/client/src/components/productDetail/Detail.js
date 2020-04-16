51e4346133c4a3f0f48aec06f2d426f39c965795
huuThien <thiennguyen.it.ute@gmail.com>
2020-04-16 23:06:41 +0700

    commit design-ui-01

--- FrontEnd/client/src/components/productDetail/Detail.js
+++ FrontEnd/client/src/components/productDetail/Detail.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import img1 from '../../images/item1.jpg';
 import img2 from '../../images/banner/empty_banner.jpg';
 import './detail.css';
+import {Tag, Alert, notification} from 'antd';
+import {PlusOutlined, MinusOutlined, CheckCircleOutlined} from '@ant-design/icons';
+
 
 export default class Detail extends Component {
     constructor(props){
@@ -9,11 +12,17 @@ export default class Detail extends Component {
         this.state={
             mainImage: img1
         }
+        
     }
     handleClickSubImage(e){
         console.log(e.target.src);
         this.setState({
             mainImage: e.target.src
+        })
+        notification.open({
+            message: 'Notification Title',
+            duration: 5,
+            icon: <CheckCircleOutlined style={{ color: '#5cb85c' }} />,
         })
     }
     render() {
@@ -42,16 +51,55 @@ export default class Detail extends Component {
                     </div>
                     
                 </div>
-                <div className="info-detail-product" style={{border: '1px solid red', width: '55%'}}>
-                    <div className="body-info-detail-product" style={{border: '1px solid green'}}>
-                        <h3>Quần tây</h3>
-                        <span>Thương hiệu: Yame</span>
+                <div className="info-detail-product">
+                    <div className="body-info-detail-product">
+                        <div>
+                            <h2 style={{color: '#af9a7d'}}>Quần tây</h2>
+                        </div>
+                        <div className="title-detail-item">
+                            <span className="title-detail"><b>Danh mục:</b></span><span>Yame</span>
+                            
+                        </div>
                         <hr/>
-                        <h4>Giá: 100,000 <b>đ</b></h4>
-                        <h4>Giá thị trường: 150,000 <b>đ</b></h4>
-                        <h5>Màu: Trắng</h5>
-                        <h5>Số lượng: </h5>
+                        
+                        <div className="title-detail-item">
+                            <span className="title-detail"><b>Giá:</b></span> 
+                            <span style={{color: '#f5222d', fontSize: '18px'}}><b>99,000 đ</b></span>
+                        </div>
+                        <div className="title-detail-item">
+                            <span className="title-detail"><b>Giá thị trường:</b></span><span style={{fontSize: '18px', textDecoration: 'line-through'}}>150,000<b> đ</b></span>
+                        </div>
+                        <div className="title-detail-item">
+                            <span className="title-detail"><b>Trạng thái:</b></span><Tag color="green"><b>active</b></Tag>
+                        </div>
+                        <div className="title-detail-item">
+                            <span className="title-detail"><b>Màu sắc:</b></span><Tag color="red"><b>red</b></Tag>
+                        </div>
+                        <div className="title-detail-item">
+                            <span className="title-detail"><b>Số lượng:</b></span> 
+                            <div style={{border: '2px solid gray', display: 'flex', fontWeight: 'bold'}}>
+                                    <div className="mount-item" style={{width: '32px', borderRight: '2px solid gray'}}>
+                                        <MinusOutlined></MinusOutlined>
+                                        </div>
+                                    <div className="mount-item" style={{width: '30px'}}>0</div>
+                                    <div className="mount-item" style={{width: '32px', borderLeft: '2px solid gray'}}>
+                                        <PlusOutlined></PlusOutlined>
+                                    </div>
+                                </div>
+                        </div>
+                        <div className="add-cart-detail-page">
+                            <button className="add-cart-btn-detail-page" onClick={() => this.handleAddCartBtn()}>
+                                Thêm giỏ hàng
+                            </button>
+                        </div>
                     </div>
+                </div>
+                <div className="info-description-detail-product">
+                    <h6 style={{color: 'red'}}>Liên hệ: 84 33 697 0895 - (7:30-22:00)</h6>
+                    <Alert className="alert-description-item" message="Hàng chất lượng chính hãng" type="success" showIcon />
+                    <Alert className="alert-description-item" message="Kiểm tra kỹ trước khi nhận hàng" type="success" showIcon />
+                    <Alert className="alert-description-item" message="30 ngày đổi trả dễ dàng" type="success" showIcon />
+                    
                 </div>
             </article>
         )
