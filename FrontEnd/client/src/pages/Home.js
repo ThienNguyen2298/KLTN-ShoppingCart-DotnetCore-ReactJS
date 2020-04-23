07634c2a3788972aba67ae040d01c96d4564da0c
huuThien <thiennguyen.it.ute@gmail.com>
2020-04-23 10:15:31 +0700

    commit continue week 4 01

--- FrontEnd/client/src/pages/Home.js
+++ FrontEnd/client/src/pages/Home.js
@@ -1,11 +1,16 @@
 import React, {useEffect} from 'react';
 import Banner from '../components/banner/Banner';
 
+
+
 import ListProducts from '../components/products/ListProducts';
+
 
 export default function Home() {
     useEffect(() => {
         window.scrollTo(0, 0)
+
+        
     }, [])
     return (
         <div style={{background: '#ebebeb'}}>
